db52d42bced1a8c81f53b99317dba948a581add8
itsm16 <111368673+itsm16@users.noreply.github.com>
2024-05-16 19:40:29 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,25 @@ import axios from 'axios'
 
 function App() {
 const [tasks, setTasks] = useState([])
+const [name, setName] = useState('');
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+      const response = await axios.post('/api/create', { name });
+      if (response.status === 201) {
+          // Do something with the response data if needed
+      } else {
+          console.error('Failed to create todo');
+      }
+  } catch (error) {
+      console.error('Error:', error);
+  }
+};
+
 
 useEffect(()=>{
-  axios.get("/api/tasks")
+  axios.get("/api/todoDb")
   .then((response)=>{setTasks(response.data)})
   .catch((err)=>{console.log(err)})
 })
@@ -21,6 +37,19 @@ useEffect(()=>{
         tasks.map((task,index)=>(<h4 key={task.id} >{task.name}</h4>))
         }
 
+      </div>
+      <div>
+      <form onSubmit={handleSubmit}>
+            <label>
+                Todo Name:
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
       </div>
     </>
   )
