c7ad9d7c7e0e97f5cc4e61f2efaba37d3d216c72
itsm16 <111368673+itsm16@users.noreply.github.com>
2024-05-16 19:38:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
-
+import connectDb from './db.js';
+import todoModel from './model.js';
+connectDb();
 const app = express();
 
 // Middlewares
 app.use(cors());
+app.use(express.json())
 
 const tasks = [
     {
@@ -21,8 +24,30 @@ const tasks = [
     }
 ]
 
+app.get("/",(req,res)=>{
+    res.send("App's Running")
+})
+
 app.get("/api/tasks",(req,res)=>{
     res.send(tasks)
+})
+
+app.get("/api/create",async (req,res)=>{
+    await todoModel.create({name: "Game"})
+    res.redirect("/")
+})
+
+app.post("/api/create",async (req,res)=>{
+    const {name} = req.body;
+    await todoModel.create({name})
+
+})
+
+app.get("/api/todoDb",async (req,res)=>{
+    await todoModel.find()
+    .then((entries)=>res.send(entries))
+    .catch((e)=>console.log(e))
+
 })
 
 app.listen(3000,()=>{
